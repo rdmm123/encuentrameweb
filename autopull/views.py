7d6e9a9a34b99474235ed8fcff85a52dcf50bde8
rdmm123 <rdmarenco@uninorte.edu.co>
2021-09-15 11:12:01 -0500

    Implemented autopull from repo

--- autopull/views.py
+++ autopull/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden, HttpResponseServerError
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 
 import requests
 from ipaddress import ip_address, ip_network
+import os
 
+@require_POST
 @csrf_exempt
 def git_pull(request):
+
+    # Verify if request came from GitHub
     forwarded_for = u'{}'.format(request.META.get('HTTP_X_FORWARDED_FOR'))
     client_ip_address = ip_address(forwarded_for)
     whitelist = requests.get('https://api.github.com/meta').json()['hooks']
@@ -16,5 +21,13 @@ def git_pull(request):
             break
     else:
         return HttpResponseForbidden('Permission denied.')
+
+    # Process the GitHub events
+    event = request.META.get('HTTP_X_GITHUB_EVENT', 'ping')
+    if event == 'push':
+        os.system("cd /home/ubuntu/encuentrameweb")
+        os.system("git pull")
+        print("Pulled most recent changes.")
+        return HttpResponse('success')
     
-    return HttpResponse('pong')
+    return HttpResponse(status=204)
