02e7488034cf49bce22abbb3cef5fd6ffc02923e
rdmm123 <rdmarenco@uninorte.edu.co>
2021-09-27 20:17:35 -0500

    Fixed minor issue in the historic section

--- locations/urls.py
+++ locations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import homepage_view, get_location
+from .views import homepage_view, get_location, get_route
 
 urlpatterns = [
     path('', homepage_view, name='home'),
     path('ajax/get_location', get_location, name='get_location'),
+    path('ajax/get_route/<str:date>', get_route, name='get_route'),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Location
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 import json
 
@@ -15,3 +15,8 @@ def get_location(request):
     
     last_location_json = json.loads(serializers.serialize('json', [last_location]))[0]
     return JsonResponse(last_location_json["fields"])
+
+def get_route(request, date):
+    route_query = Location.objects.filter(date=date)
+    route_json = {'route': list(route_query.values())}
+    return  JsonResponse(route_json, safe=False)
