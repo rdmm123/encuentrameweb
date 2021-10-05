bed34c2507ad510d914a3faf9169b32c0476525c
rdmm123 <rdmarenco@uninorte.edu.co>
2021-10-04 21:41:10 -0500

    Updated UDP server to write on both main and backup databases

--- udp_server.py
+++ udp_server.py
@@ -5,15 +5,33 @@ from twisted.internet import reactor
 from decouple import config
 from datetime import datetime
 
-def connect_db():
+def connect_db(host, user, pwd, db):
     con = MySQLdb.connect(
-        host=config("DB_HOST"),
-        user=config("DB_USER"),
-        passwd=config("DB_PASS"),
-        db=config("DB_NAME"),
+        host=host,
+        user=user,
+        passwd=pwd,
+        db=db,
     )
     cur = con.cursor()
     return [con, cur]
+
+def add_data(db_info, location):
+    host, user, pwd, db = db_info
+    con, cur = connect_db(host, user, pwd, db)
+        
+    cur.execute('SELECT id FROM locations_location')
+
+    ids = [idset[0] for idset in cur.fetchall()]
+    if ids:
+        newid = ids[-1] + 1
+    else:
+        newid = 1
+    
+    timestamp_str = location[2] + " " + location[3]
+    cur.execute(f"INSERT INTO locations_location VALUES ('{newid}', '{location[0]}', '{location[1]}', '{timestamp_str}')")
+    con.commit()
+    cur.close()
+    con.close()
 
 port = 3001
 
@@ -28,24 +46,8 @@ class Server(DatagramProtocol):
         data_str = data.decode('utf-8')
         print(f"Received data from {addr}:\n\t{data_str}")
         location = data_str.split(",")
-
-        con, cur = connect_db()
-        
-        cur.execute('SELECT id FROM locations_location')
-
-        ids = [idset[0] for idset in cur.fetchall()]
-        if ids:
-            newid = ids[-1] + 1
-        else:
-            newid = 1
-        
-        timestamp_str = location[2] + " " + location[3]
-
-        cur.execute(f"INSERT INTO locations_location VALUES ('{newid}', '{location[0]}', '{location[1]}', '{timestamp_str}')")
-        con.commit()
-        cur.close()
-        con.close()
-
+        add_data([config("MAIN_HOST"), config("MAIN_USER"), config("MAIN_PASS"), config("MAIN_NAME")], location)
+        add_data([config("BACKUP_HOST"), config("BACKUP_USER"), config("BACKUP_PASS"), config("BACKUP_NAME")], location)
 
 reactor.listenUDP(port, Server())
 print(f"Listening on 0.0.0.0:{port}")
