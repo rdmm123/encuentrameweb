4b329dd0fe6e1fe03f8acefac1393fe068bb9a80
rdmm123 <rdmarenco@uninorte.edu.co>
2021-10-18 21:49:44 -0500

    Added license plate model

--- locations/models.py
+++ locations/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Location(models.Model):
     latitude = models.CharField(max_length=50)
     longitude = models.CharField(max_length=50)
+    plate = models.CharField(max_length=50)
     timestamp = models.DateTimeField()
